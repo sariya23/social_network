d6e164d1e0bc5b3e764135fccaa816c41f7c8504
sariya <dayzwaitik123@yandex.ru>
2024-06-26 17:25:12 +0300

    Change password

--- social_network/account/urls.py
+++ social_network/account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 
 from . import views
@@ -9,5 +9,7 @@ app_name = 'account'
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(extra_context={"title": "Login"}), name="login"),
     path("logout/", views.logout_user, name="logout"),
+    path("password-change/", views.CustomPasswordChange.as_view(), name="password_change"),
+    path("password-change/done/", auth_views.PasswordChangeDoneView.as_view() ,name="password_change_done"),
     path("", views.dashboard, name="dashboard"),
 ]
--- social_network/account/views.py
+++ social_network/account/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.views import PasswordChangeDoneView, PasswordChangeView
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.contrib.auth import logout
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import login_required
 
 @login_required
@@ -12,3 +13,7 @@ def dashboard(request: HttpRequest) -> HttpResponse:
 def logout_user(request: HttpRequest) -> HttpResponse:
     logout(request)
     return HttpResponseRedirect(reverse("account:dashboard"))
+
+
+class CustomPasswordChange(PasswordChangeView):
+    success_url = reverse_lazy("account:password_change_done")
