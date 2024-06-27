08a43ca906354f7edb311e17bd2e6d3f0841c813
sariya <dayzwaitik123@yandex.ru>
2024-06-27 17:52:47 +0300

    add verbose name

--- social_network/account/models.py
+++ social_network/account/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name='profile')
     date_of_birth = models.DateTimeField(blank=True, null=True)
     photo = models.ImageField(upload_to="users/%Y/%m/%d", blank=True)
 
