af3e60a9f5f03035313aca5428e934fffe5fa0c7
sariya <dayzwaitik123@yandex.ru>
2024-07-01 20:59:00 +0300

    add content type

--- social_network/actions/models.py
+++ social_network/actions/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+
+class Action(models.Model):
+    class Meta:
+        indexes = [
+            models.Index(fields=["-created"]),
+            models.Index(fields=["target_content_type", "target_id"]),
+        ]
+        ordering = ["-created"]
+    user = models.ForeignKey("auth.User", related_name="actions", on_delete=models.CASCADE)
+    verb = models.CharField(max_length=300)
+    created = models.DateTimeField(auto_now_add=True)
+    target_content_type = models.ForeignKey(ContentType, blank=True, null=True, on_delete=models.CASCADE, related_name="target_obj")
+    target_id = models.PositiveIntegerField(null=True, blank=True)
+    target = GenericForeignKey("target_content_type", "target_id")
--- social_network/social_network/settings.py
+++ social_network/social_network/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     "django_extensions",
     "images.apps.ImagesConfig",
     "easy_thumbnails",
+    "actions.apps.ActionsConfig",
 ]
 
 MIDDLEWARE = [
